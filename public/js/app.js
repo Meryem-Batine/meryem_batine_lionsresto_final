ac69c9131d56ef25087d7cd6facf8d013ab030f6
Meryem-Batine <meryemnour200@gmail.com>
2023-06-23 14:21:05 +0100

    nineteen

---
+++ public/js/app.js
@@ -0,0 +1,50 @@
+//! vedio 1 ///////////////////////////////////////////////
+const openVideo=()=>
+{
+contenu.classList.add("show");
+}
+
+const closeVideo=()=>
+{
+contenu.classList.remove("show");
+}
+
+let watchvideo= document.querySelector("#watchvideo");
+let contenu =document.querySelector(".video_f");
+let ferme = document.querySelector(".btn-close-vd");
+watchvideo.addEventListener("click",openVideo)
+ferme.addEventListener("click",closeVideo);
+//! carousel /////////////////////////////////////////////////////
+let carusel = document.querySelector(".carusel");
+let isDrag = false,
+  startX,
+  startScrollLeft;
+
+let slidBtn = document.querySelectorAll(".btnsslid button");
+
+
+let firstWidth = document.querySelector(".card").offsetWidth;
+slidBtn.forEach(btn => {
+  btn.addEventListener('click',()=>{
+    carusel.scrollLeft += btn.id === "slidBtn1" ? -firstWidth : firstWidth;
+
+})
+});
+
+const dragStart = (e) => {
+  isDrag = true;
+  carusel.classList.add("draging");
+  startX = e.pageX;
+  startScrollLeft = carusel.scrollLeft;
+};
+const draga = (e) => {
+  if (!isDrag) return;
+  carusel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+const dragStop = (e) => {
+  isDrag = false;
+  carusel.classList.remove("draging");
+};
+carusel.addEventListener("mouseover", draga);
+carusel.addEventListener("mousedown", dragStart);
+document.addEventListener("mouseup", dragStop);
