86db54c5c0333ea3095e3fb549bb35b4c71136c1
Meryem-Batine <meryemnour200@gmail.com>
2023-06-23 16:53:47 +0100

    twenty two

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,18 @@
+let scroll_up = document.querySelector(".up")
+window.onscroll = function () {
+    if (this.scrollY >= 500) {
+        scroll_up.classList.add("show")
+    } else {
+        scroll_up.classList.remove("show")
+
+    }
+}
+scroll_up.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+})
 //^ connexion //////////////////
 let connexion = document.querySelector(".conx-1");
 console.log(connexion);
